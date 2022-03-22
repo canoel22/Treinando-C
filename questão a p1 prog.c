deb663e27873d7cc7d01749c8045024388be8e41
Carine Noel <86012254+canoel22@users.noreply.github.com>
2022-03-22 16:26:41 -0300

    Add files via upload

---
+++ questão a p1 prog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int i, n, cont=0, num[n]; // num[n] pro vetor ficar do tamanho certo, sem ter espaço a toa
+    
+    scanf ("%d", &n); 
+    for (i=0; i<n; i++){
+        scanf ("%d", &num[i]); //para receber vários valores em um vetor
+        if (num[i]%2==0){
+            while (num[i]%2==0){ //para achar quantas vezes o número x é divisível por 2
+                num[i]=num[i]/2;
+                cont++;
+            }
+        }  
+    }
+    printf ("%d", cont); //quantas vezes os números foram divididos por 2
+    return 0;
+}
+
